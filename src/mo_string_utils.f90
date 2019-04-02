dc2944cf1496aa66389d773508692d20fa7eb956
Robert Schweppe <robert.schweppe@ufz.de>
2019-04-02 22:54:51 +0200

    - created a simple script to visualize the dependencies of the data arrays

--- src/mo_string_utils.f90
+++ src/mo_string_utils.f90
@@ -297,28 +297,36 @@ CONTAINS
     ! replaces proper words only,
     ! e.g. replace_word('our hour', 'our', 'their') --> 'their hour'
     ! written Nov 2018 by Robert Schweppe
-    FUNCTION replace_word(s, word, rep)  RESULT(outs)
+    FUNCTION replace_word(s, word, rep, check_negative_number_arg)  RESULT(outs)
       CHARACTER(*) :: s, word, rep
+      logical, optional  :: check_negative_number_arg
       CHARACTER(LEN(s)+100) :: outs     ! provide outs with extra 100 char len
       INTEGER               :: i, nt, nr
-      
+
       outs = s ; nt = LEN_TRIM(word) ; nr = LEN_TRIM(rep)
       DO
-        i = index_word(outs, word(:nt))
+        i = index_word(outs, word(:nt), check_negative_number_arg)
         if (i == 0) exit
         outs = outs(:i-1) // rep(:nr) // outs(i+nt:)
       END DO
     END FUNCTION replace_word
 
   ! written Nov 2018 by Robert Schweppe
-  function index_word(s, text) result(out_index)
+  function index_word(s, text, check_negative_number_arg) result(out_index)
     CHARACTER(*)       :: s
     CHARACTER(*)       :: text
+    logical, optional  :: check_negative_number_arg
     integer :: out_index
 
     integer :: i, nw, ns, i_add
-    logical :: is_begin_not_word
+    logical :: is_begin_not_word, check_negative_number_default
     character(63), parameter :: word_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_'
+    character(10), parameter :: digit_chars = '0123456789'
+
+    check_negative_number_default = .false.
+    if (present(check_negative_number_arg)) then
+      check_negative_number_default = check_negative_number_arg
+    end if
 
     nw = LEN_TRIM(text) ; ns = LEN_TRIM(s)
     out_index = 0
@@ -337,6 +345,10 @@ CONTAINS
         if (i-1 > 0) then
           ! is the word preceded by a alphanumeric character?
           if (scan(s(i-1:i-1), word_chars) == 1) then
+            is_begin_not_word = .false.
+          else if (check_negative_number_default .and. &
+                   scan(s(i-1:i-1), '-') == 1 .and. &
+                   scan(digit_chars, text(1:1)) > 0 ) then
             is_begin_not_word = .false.
           end if
         end if
