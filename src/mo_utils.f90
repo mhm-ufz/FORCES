70c702db5dda13500074a214514d1066f785b730
Robert Schweppe <robert.schweppe@ufz.de>
2020-05-27 17:20:58 +0200

    - fixed a bug (integral_coordinate not added in aliases) - removed some status prints

--- src/mo_utils.f90
+++ src/mo_utils.f90
@@ -1387,7 +1387,6 @@ CONTAINS
       ! this is the indexer for the vector
       counts = count(mask(indexMin: indexMax), kind=i8)
       ! unpack slices of maximum size
-      print*, currentCounts, currentCounts + counts - 1_i8, indexMin, indexMax
       if (counts == (indexMax - indexMin + 1_i8)) then
         unpacked(indexMin: indexMax) = vector(currentCounts: currentCounts + counts - 1_i8)
       else if (counts == 0_i8) then
