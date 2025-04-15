0639b86a5b7ae17bac37933fd437a91474ece211
Sebastian Müller <mueller.seb@posteo.de>
2025-04-15 14:26:05 +0200

    mo_list: doc indent fix

--- src/mo_list.f90
+++ src/mo_list.f90
@@ -10,46 +10,46 @@
 !! to contain any type of data. The `key` can be an integer(i4), string, or
 !! any user-defined \ref key_class.
 !!
-!!  Example for an integer list with a specific getter:
-!!  \code{.f90}
-!!  module mo_int_list
-!!    use mo_list, only: list
-!!    type, extends(list) :: integer_list
-!!    contains
-!!      procedure :: get_integer
-!!    end type
-!!  contains
-!!    subroutine get_integer(this, key, value)
-!!      class(integer_list), intent(in) :: this
-!!      class(*), intent(in) :: key
-!!      integer, pointer, intent(out) :: value
-!!      class(*), pointer :: p
-!!      call this%get(key, p)
-!!      if (associated(p)) then
-!!        select type (p)
-!!          type is (integer)
-!!            value => p
-!!          class default
-!!            error stop 'list item not an integer'
-!!        end select
-!!      else
-!!        value => null()
-!!      end if
-!!    end subroutine get_integer
-!!  end module mo_int_list
+!! Example for an integer list with a specific getter:
+!! \code{.f90}
+!! module mo_int_list
+!!   use mo_list, only: list
+!!   type, extends(list) :: integer_list
+!!   contains
+!!     procedure :: get_integer
+!!   end type
+!! contains
+!!   subroutine get_integer(this, key, value)
+!!     class(integer_list), intent(in) :: this
+!!     class(*), intent(in) :: key
+!!     integer, pointer, intent(out) :: value
+!!     class(*), pointer :: p
+!!     call this%get(key, p)
+!!     if (associated(p)) then
+!!       select type (p)
+!!         type is (integer)
+!!           value => p
+!!         class default
+!!           error stop 'list item not an integer'
+!!       end select
+!!     else
+!!       value => null()
+!!     end if
+!!   end subroutine get_integer
+!! end module mo_int_list
 !!
-!!  program test_int_list
-!!    use mo_int_list
-!!    type(integer_list) :: ilist
-!!    integer, pointer :: val
-!!    call ilist%add_clone('age', 30)
-!!    call ilist%add_clone('year', 1990)
-!!    call ilist%get_integer('age', val)
-!!    print*, 'age: ', val
-!!    call ilist%get_integer('year', val)
-!!    print*, 'year: ', val
-!!  end program test_int_list
-!!  \endcode
+!! program test_int_list
+!!   use mo_int_list
+!!   type(integer_list) :: ilist
+!!   integer, pointer :: val
+!!   call ilist%add_clone('age', 30)
+!!   call ilist%add_clone('year', 1990)
+!!   call ilist%get_integer('age', val)
+!!   print*, 'age: ', val
+!!   call ilist%get_integer('year', val)
+!!   print*, 'year: ', val
+!! end program test_int_list
+!! \endcode
 !!
 !> \copyright flist was originally released under the BSD 3-Clause license (included below).
 !!
@@ -80,6 +80,7 @@
 !! ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 !! (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 !! SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!!
 !> \version 0.1
 !> \authors Jacob Williams, Sebastian Mueller
 !> \date    Apr 2025
